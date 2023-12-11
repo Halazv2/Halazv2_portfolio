15f06d228629ba53968728399c43201ddd046771
Halazv2 <halaziani2003@gmail.com>
2023-12-11 16:05:28 +0100

    Add gitignore for .env file and update target to es6 in tsconfig.json

---
+++ prettier.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('prettier').Options} */
+module.exports = {
+    singleQuote: true,
+    semi: false,
+    trailingComma: 'all',
+    
+    plugins: ['prettier-plugin-tailwindcss'],
+  }
+  
