1cc5d6da0d522977cc75a9e8124bbdf807ace0db
Halazv2 <halaziani2003@gmail.com>
2023-12-11 16:14:44 +0100

    Add: header components

---
+++ src/components/HeaderCompoents/Avatar.tsx
@@ -0,0 +1,48 @@
+import clsx from 'clsx'
+import Image from 'next/image'
+import Link from 'next/link'
+import avatarImage from '@/assets/images/avatar.jpeg'
+import React from 'react'
+
+export function Avatar({
+  large = false,
+  className,
+  ...props
+}: Omit<React.ComponentPropsWithoutRef<typeof Link>, 'href'> & {
+  large?: boolean
+}) {
+  return (
+    <Link
+      href="/"
+      aria-label="Home"
+      className={clsx(className, 'pointer-events-auto')}
+      {...props}
+    >
+      <Image
+        src={avatarImage}
+        alt=""
+        sizes={large ? '4rem' : '2.25rem'}
+        className={clsx(
+          'rounded-full bg-zinc-100 object-cover dark:bg-zinc-800',
+          large ? 'h-16 w-16' : 'h-9 w-9',
+        )}
+        priority
+      />
+    </Link>
+  )
+}
+
+export function AvatarContainer({
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<'div'>) {
+  return (
+    <div
+      className={clsx(
+        className,
+        'h-10 w-10 rounded-full bg-white/90 p-0.5 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:ring-white/10',
+      )}
+      {...props}
+    />
+  )
+}
