e254aab5c33f8e65a7047a72e36117ef0b3ebaa7
Halazv2 <halaziani2003@gmail.com>
2023-12-11 19:54:38 +0100

    replace image

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -2,7 +2,7 @@ import image1 from '@/assets/images/image-1.png'
 import image2 from '@/assets/images/image-2.png'
 import image3 from '@/assets/images/image-3.png'
 import image4 from '@/assets/images/image-4.png'
-import image5 from '@/assets/images/image-5.jpg'
+import image5 from '@/assets/images/image-5.jpeg'
 import portrait from '@/assets/images/portrait.jpg'
 import avatar from '@/assets/images/avatar.jpeg'
 
