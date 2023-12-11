191fccb62249e8674616233dccc7ab73ff5ecf70
Halazv2 <halaziani2003@gmail.com>
2023-12-11 16:08:54 +0100

    Add constants

---
+++ src/helpers/constants.ts
@@ -0,0 +1,56 @@
+import { Role, languageIconProps } from '@/types/interfaces'
+import {
+  logoAyouris,
+  logoMysestAi,
+  logoV12software,
+  logoYoucode,
+} from '@/assets/svg/companies_logos'
+import {
+  JavaScript,
+  Keycloak,
+  TypeScript,
+  Vue,
+} from '@/assets/svg/languages_logos'
+
+export const resume: Array<Role> = [
+  {
+    company: 'V12Software',
+    title: 'Full Stack Developer',
+    logo: logoV12software,
+    start: 'Aug 2023',
+    end: {
+      label: 'Present',
+      dateTime: new Date().getFullYear().toString(),
+    },
+  },
+  {
+    company: 'MyRestai Europe',
+    title: 'Full Stack Developer',
+    logo: logoMysestAi,
+    start: 'Dec 2022',
+    end: 'Aug 2023',
+  },
+  {
+    company: 'Ayouris',
+    title: 'Frontend Web Developer - Internship',
+    logo: logoAyouris,
+    start: 'Jul 2022',
+    end: 'Aug 2022',
+  },
+  {
+    company: 'YouCode Maroc',
+    title: 'Certified Full Stack JavaScript Developer',
+    logo: logoYoucode,
+    start: 'Oct 2021',
+    end: 'Oct 2023',
+  },
+]
+
+export const languageIcons: languageIconProps = {
+  JavaScript: JavaScript,
+  TypeScript: TypeScript,
+  Vue: Vue,
+  Keycloak: Keycloak,
+  HTML: Keycloak,
+}
+
