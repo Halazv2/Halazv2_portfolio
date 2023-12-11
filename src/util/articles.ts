c4b99dba82742733288c6394e8d45e676b858f6a
Halazv2 <halaziani2003@gmail.com>
2023-12-11 16:09:26 +0100

    Add: import and sort articles

---
+++ src/util/articles.ts
@@ -0,0 +1,26 @@
+import { Article, ArticleWithSlug } from '@/types/interfaces'
+import glob from 'fast-glob'
+
+async function importArticle(
+  articleFilename: string,
+): Promise<ArticleWithSlug> {
+  let { article } = (await import(`../app/articles/${articleFilename}`)) as {
+    default: React.ComponentType
+    article: Article
+  }
+
+  return {
+    slug: articleFilename.replace(/(\/page)?\.mdx$/, ''),
+    ...article,
+  }
+}
+
+export async function getAllArticles() {
+  let articleFilenames = await glob('*/page.mdx', {
+    cwd: './src/app/articles',
+  })
+
+  let articles = await Promise.all(articleFilenames.map(importArticle))
+
+  return articles.sort((a, z) => +new Date(z.date) - +new Date(a.date))
+}
